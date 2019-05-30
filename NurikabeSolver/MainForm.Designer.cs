123fad38a787fe3969e4d7bfdc12746eb60fbc39
Marcin Wiktorowski <wiktorowski211@gmail.com>
2019-05-30 21:03:09 +0200

    Init commit

--- NurikabeSolver/MainForm.Designer.cs
+++ NurikabeSolver/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NurikabeSolver
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 600);
             this.Text = "Form1";
         }
 
