05f07ea80fd65ce42e5ee1561e22f6fce79d4bae
filipwicha <33604199+filipwicha@users.noreply.github.com>
2019-09-16 15:03:52 +0200

    Update Program.cs

--- NurikabeSolver/Program.cs
+++ NurikabeSolver/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,13 +12,19 @@ namespace NurikabeSolver
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(/*int size, int[,] island_list, int[,] result_list*/)
         {
             var controller = new Controller();
 
+            // ------ to delete
+            var size =  5;
+            var island_list = new int[4, 3] { { 1, 3, 3 }, { 2, 2, 3 }, { 5, 1, 2 }, { 5, 5, 4 } };
+            var result_list = new int[5, 5] { { 0, 0, 1, 1, 1 }, { 0, 1, 0, 0, 0 }, { 0, 1, 1, 0, 1 }, { 0, 0, 0, 0, 1 }, { 1, 1, 0, 1, 1 } };
+            // ------
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new View(controller));
+            Application.Run(new View(controller, size, island_list, result_list));
         }
     }
 }
